4b11f59f691dfe8fcbb219af7b5cbb3f80d22d69
litt1e-p <free9fw@gmail.com>
2016-10-31 17:14:31 +0800

    swift 3 compatible

--- UIWindow-Transition-Sample/UIWindow-Transition-Sample/UIWindow-Transition/UIWindow+Transition.swift
+++ UIWindow-Transition-Sample/UIWindow-Transition-Sample/UIWindow-Transition/UIWindow+Transition.swift
@@ -71,7 +71,7 @@ public extension UIWindow
         let durationMax: NSTimeInterval = duration != nil ? duration! : 0.50
         switch style {
         case .ZoomOut:
-            let snapshot:UIView = snapshotViewAfterScreenUpdates(true)
+            let snapshot:UIView = snapshotViewAfterScreenUpdates(true)!
             toVc.view.addSubview(snapshot)
             rootViewController = toVc
             UIView.animateWithDuration(durationMin, animations: {() in
@@ -82,7 +82,7 @@ public extension UIWindow
                     snapshot.removeFromSuperview()
             })
         case .ZoomIn:
-            let snapshot:UIView = snapshotViewAfterScreenUpdates(true)
+            let snapshot:UIView = snapshotViewAfterScreenUpdates(true)!
             toVc.view.addSubview(snapshot)
             rootViewController = toVc
             UIView.animateWithDuration(durationMax, animations: {() in
@@ -93,7 +93,7 @@ public extension UIWindow
                     snapshot.removeFromSuperview()
             })
         case .Dissolve:
-            let snapshot:UIView = snapshotViewAfterScreenUpdates(true)
+            let snapshot:UIView = snapshotViewAfterScreenUpdates(true)!
             toVc.view.addSubview(snapshot)
             rootViewController = toVc
             UIView.animateWithDuration(durationMin, animations: {
